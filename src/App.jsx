3609de1e0385108d101039344879b53bcd3489f4
seezmashele <seez.mash@hotmail.com>
2023-06-01 20:21:31 +0200

    added top50 page

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom"
 import Home from "./pages/Home"
 import Playlist from "./pages/Playlist"
 import Spotlight from "./pages/Spotlight"
+import Top50 from "./pages/Top50"
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Route path="/" element={<Home />} />
       <Route path="/playlist" element={<Playlist />} />
       <Route path="/spotlight" element={<Spotlight />} />
+      <Route path="/top50" element={<Top50 />} />
       <Route path="*" element={<Home />} />
     </Routes>
   )
--- src/shared/pageButtons.js
+++ src/shared/pageButtons.js
@@ -1,5 +1,6 @@
 export const pageButtons = [
   { title: "Album", path: "/" },
   { title: "Playlist", path: "/playlist" },
-  { title: "Spotlight", path: "/spotlight" }
+  { title: "Spotlight", path: "/spotlight" },
+  { title: "Top50", path: "/top50" }
 ]
